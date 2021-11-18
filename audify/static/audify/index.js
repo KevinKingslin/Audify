f339a0adb63a55394a8454fdea2c34ef9d2f2411
Kevin Kingslin <kevinkingslin@gmail.com>
2021-11-18 10:40:59 +0530

    Added javascript to layout.html

---
+++ audify/static/audify/index.js
@@ -0,0 +1,8 @@
+shuffle = document.querySelector('#shuffle')
+skipPrevious = document.querySelector('#skip-previous')
+play = document.querySelector('#play')
+skipNext = document.querySelector('#skip-next')
+repeat = document.querySelector('#repeat')
+
+document.addEventListener('DOMContentLoaded', () => {
+})
