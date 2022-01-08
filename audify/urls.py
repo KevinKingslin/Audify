995f5a0d7bbe3ac96b2408cc6dfd62201c67253c
Kevin Kingslin <kevinkingslin@gmail.com>
2022-01-08 18:33:24 +0530

    Added search route

--- audify/urls.py
+++ audify/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="login"),
-    path("login", views.login_view, name="index"),
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
+    path('search', views.search, name="search")
 ]
